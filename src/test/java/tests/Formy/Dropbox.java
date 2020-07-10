3746b2c46b04f267b8be3101d3958b56e8df20c8
Bilal USLU <uslubilal45@gmail.com>
2020-07-09 20:51:53 -0400

    dropdown title verify

--- src/test/java/tests/Formy/Dropbox.java
+++ src/test/java/tests/Formy/Dropbox.java
@@ -1,5 +1,6 @@
 package tests.Formy;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormyHomePage;
 import utils.Driver;
@@ -14,7 +15,23 @@ public class Dropbox {
         FormyHomePage formyHomePage = new FormyHomePage();
         Driver.get().get(formyHomePage.url);
 
-        clickOnTheDesiredLink1("Checkbox");
+        clickOnTheDesiredLink1("Dropbox");
+
+
+        Driver.get().close();
+    }
+
+    @Test
+    public void verify_button_link_page_title() throws Exception {
+
+        FormyHomePage formyHomePage = new FormyHomePage();
+        Driver.get().get(formyHomePage.url);
+
+        clickOnTheDesiredLink1("Dropbox");
+
+        Assert.assertEquals(Driver.get().getCurrentUrl().contains("dropdown"),true);
+
+
 
         Driver.get().close();
     }
