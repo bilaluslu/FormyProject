512784591e4e8ea91c0440f42a59f5799451da55
Bilal USLU <uslubilal45@gmail.com>
2020-07-09 21:32:15 -0400

    verify radio button

--- src/test/java/tests/Formy/RadioButton.java
+++ src/test/java/tests/Formy/RadioButton.java
@@ -1,5 +1,6 @@
 package tests.Formy;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormyHomePage;
 import utils.Driver;
@@ -15,6 +16,19 @@ public class RadioButton {
         Driver.get().get(formyHomePage.url);
 
         clickOnTheDesiredLink1("Radio Button");
+
+        Driver.get().close();
+    }
+
+    @Test
+    public void verify_button_link_page_1() throws Exception {
+
+        FormyHomePage formyHomePage = new FormyHomePage();
+        Driver.get().get(formyHomePage.url);
+
+        clickOnTheDesiredLink1("Radio Button");
+
+        Assert.assertEquals(Driver.get().getCurrentUrl() , "http://formy-project.herokuapp.com/radiobutton");
 
         Driver.get().close();
     }
